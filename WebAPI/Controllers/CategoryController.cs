11a97d07400365b5f215fd7dfd37cbadb99092d2
Danny Tim <dantin241@gmail.com>
2020-02-10 15:58:28 +0200

    Importing project

---
+++ WebAPI/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class CategoryController : ApiController
+    {
+        private DBModel db = new DBModel();
+
+        // GET: api/Category
+        public IQueryable<Category> GetCategories()
+        {
+            return db.Categories;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
